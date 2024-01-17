4427300d9220438262988850572aeddd0a033123
sergio lopez~ <88870495+slopez2020276@users.noreply.github.com>
2024-01-17 16:27:25 -0600

    Img de Linea Tiempo Funcional

--- img-optimizadas/src/routes/noticias.routes.js
+++ img-optimizadas/src/routes/noticias.routes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const NoticiasController = require('../controllers/noticias.controller');
 const md_auteticacion = require('../middlewares/autenticacion')
 const api = express.Router();
+const multer = require('../../libs/multer');
+
 
 api.get('/ObtnerNoticias',NoticiasController.obtenerNoticias);
 api.put('/editarNoticiasxId/:idNoticia',md_auteticacion.Auth,NoticiasController.editarNoticias)
 api.delete('/eliminarNoticia/:idNoticia',md_auteticacion.Auth,NoticiasController.eliminarNoticias)
 api.get('/obtenernoticiasid/:idNoticia',NoticiasController.buscarNoticasxId)
-api.post('/agregarNOticias',NoticiasController.agregarNoticias)
+api.post('/agregarNOticias',multer.single('ImgPathLineaTiempo'),NoticiasController.agregarNoticias)
 
 
 
